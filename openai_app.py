c5f1957c4c99d7c0cc3c2009d3a9e7edd3d7e77a
Taqi Haider <taqihaider592@gmail.com>
2023-03-08 23:45:41 +0500

    API key updated

--- openai_app.py
+++ openai_app.py
@@ -6,7 +6,7 @@ from langchain import OpenAI, VectorDBQA
 from langchain.document_loaders import DirectoryLoader
 from PIL import Image
 
-api_key = st.secrets["Openai_API_key"]
+api_key = st.secrets["OPENAI_API_KEY"]
 
 # Set up Streamlit app
 image= Image.open("app_banner.png")
